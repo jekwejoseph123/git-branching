d32bb2151fb64f90d5f8b885b25322cac4a83807
Winifred Epetuku <epetukuwinifred@gmail.com>
2024-04-03 11:28:34 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
      <Sidebar/>
+     <Navbar/>
     </div>
   );
 }
